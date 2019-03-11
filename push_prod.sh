5ad3788580d36716f40ad811e54c74e9e44a0840
Shakti Singh <shaktiug@gmail.com>
2019-03-11 23:00:44 +0530

    updated scripts

--- push_prod.sh
+++ push_prod.sh
@@ -6,17 +6,17 @@ then
 echo " Backing up the deployed ${config_type} "
 if [ -f /var/www/html/${config_type} ];
 then
-ssh -i dev-aws.pem ec2-user@34.222.1.157 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.222.1.157 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
 fi
 echo " Deploying the new ${config_type} "
 
-scp -i dev-aws.pem ${WORKSPACE}/${config_type} ec2-user@34.222.1.157:/tmp
-ssh -i dev-aws.pem ec2-user@34.222.1.157 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
-ssh -i dev-aws.pem ec2-user@34.222.1.157 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
+scp -i /var/lib/jenkins/dev-aws.pem ${WORKSPACE}/${config_type} ec2-user@34.222.1.157:/tmp
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.222.1.157 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.222.1.157 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
 
 echo " Restarting dispatcher 1 "
 
-ssh -i dev-aws.pem ec2-user@34.222.1.157 "sudo service httpd restart"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.222.1.157 "sudo service httpd restart"
 
 if [ "${instance}" =  "dispatcher_2" ];
 then
@@ -25,14 +25,14 @@ then
 echo " Backing up the deployed ${config_type} "
 if [ -f /var/www/html/${config_type} ];
 then
-ssh -i dev-aws.pem ec2-user@34.212.29.187 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.212.29.187 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
 fi
 echo " Deploying the new ${config_type} "
 
-scp -i dev-aws.pem ${WORKSPACE}/${config_type} ec2-user@34.212.29.187:/tmp
-ssh -i dev-aws.pem ec2-user@34.212.29.187 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
-ssh -i dev-aws.pem ec2-user@34.212.29.187 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
+scp -i /var/lib/jenkins/dev-aws.pem ${WORKSPACE}/${config_type} ec2-user@34.212.29.187:/tmp
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.212.29.187 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.212.29.187 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
 
 echo " Restarting dispatcher 1 "
 
-ssh -i dev-aws.pem ec2-user@34.212.29.187 "sudo service httpd restart"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@34.212.29.187 "sudo service httpd restart"
--- push_uat.sh
+++ push_uat.sh
@@ -5,14 +5,14 @@ then
 echo " Backing up the deployed ${config_type} "
 if [ -f /var/www/html/${config_type} ];
 then
-ssh -i dev-aws.pem ec2-user@52.12.160.247 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
 fi
 echo " Deploying the new ${config_type} "
 
-scp -i dev-aws.pem ${WORKSPACE}/${config_type} ec2-user@52.12.160.247:/tmp
-ssh -i dev-aws.pem ec2-user@52.12.160.247 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
-ssh -i dev-aws.pem ec2-user@52.12.160.247 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
+scp -i /var/lib/jenkins/dev-aws.pem ${WORKSPACE}/${config_type} ec2-user@52.12.160.247:/tmp
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
 
 echo " Restarting dispatcher 1 "
 
-ssh -i dev-aws.pem ec2-user@52.12.160.247 "sudo service httpd restart"
+ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo service httpd restart"
