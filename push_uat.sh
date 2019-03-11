a16a11ca6c3d22cb83b6b16f1ece6da22388e8fd
Shakti Singh <shaktiug@gmail.com>
2019-03-11 23:27:02 +0530

    updated script

--- push_uat.sh
+++ push_uat.sh
@@ -6,17 +6,17 @@ then
 echo " Backing up the deployed ${config_type} "
 if [ -f /var/www/html/${config_type} ];
 then
-ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
+ssh -i /var/lib/jenkins/dev-aws.pem -o StrictHostKeyChecking=no ec2-user@52.12.160.247 "sudo cp /var/www/html/${config_type} /var/www/html/${config_type}.bak"
 fi
 echo " Deploying the new ${config_type} "
 
-scp -i /var/lib/jenkins/dev-aws.pem ${WORKSPACE}/${config_type} ec2-user@52.12.160.247:/tmp
-ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
-ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
+scp -i /var/lib/jenkins/dev-aws.pem -o StrictHostKeyChecking=no ${WORKSPACE}/${config_type} ec2-user@52.12.160.247:/tmp
+ssh -i /var/lib/jenkins/dev-aws.pem -o StrictHostKeyChecking=no ec2-user@52.12.160.247 "sudo mv /tmp/${config_type} /var/www/html/${config_type}"
+ssh -i /var/lib/jenkins/dev-aws.pem -o StrictHostKeyChecking=no ec2-user@52.12.160.247 "sudo chown root:root /var/www/html/${config_type}; sudo chmod 644 /var/www/html/${config_type}"
 
 echo " Restarting dispatcher 1 "
 
-ssh -i /var/lib/jenkins/dev-aws.pem ec2-user@52.12.160.247 "sudo service httpd restart"
+ssh -i /var/lib/jenkins/dev-aws.pem -o StrictHostKeyChecking=no ec2-user@52.12.160.247 "sudo service httpd restart"
 fi
 fi
 
